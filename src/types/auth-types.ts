6facca5befe79303c33e82ca1bdd1b74fa186c20
chetanv18~ <chetan@gully2global.com>
2025-07-10 16:16:23 +0530

    login api done

---
+++ src/types/auth-types.ts
@@ -0,0 +1,28 @@
+// Authentication request and response types
+
+export interface LoginRequest {
+  email: string
+  password: string
+}
+
+export interface User {
+  _id: string
+  email: string
+  password: string
+  phone_Number: string
+  role: string
+  address: any[]
+  __v: number
+  last_login: string
+  vehicle_details: any[]
+}
+
+export interface LoginResponse {
+  success: boolean
+  message: string
+  data: {
+    user: User
+    token: string
+  }
+}
+
