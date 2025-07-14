2579cd672decfe3c6da80fa23e5c1a1c354c68ef
Uttam Shetty <uttamshetty126@gmail.com>
2025-07-14 18:47:23 +0530

    IconChanges

--- src/components/ui/TicketIcon.tsx
+++ src/components/ui/TicketIcon.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Ticket from '../../../public/assets/Ticket.svg';
 import Box from '../../../public/assets/Box.svg'
 import Editicon from '../../../public/assets/EditIcon.svg'
+import User from '../../../public/assets/user.svg'
+import widget from '../../../public/assets/Widget 5.svg'
 
 const TicketIcon = () => (
   <Image src={Ticket} alt='ticket' width={24} height={24} />
@@ -11,6 +13,12 @@ export const BoxIcon = () => (
 );
 export const EditIcon = () => (
   <Image src={Editicon} alt='box' width={24} height={24} />
+);
+export const userIcon = () => (
+  <Image src={User} alt='user' width={24} height={24} />
+);
+export const DashboardIcon = () => (
+  <Image src={widget} alt='user' width={24} height={24} />
 );
 
 export default TicketIcon;
