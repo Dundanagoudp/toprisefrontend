ea3c8b8d258adc69a2fb0455eac2e23cbc05af96
Uttam Shetty <uttamshetty126@gmail.com>
2025-07-15 14:29:50 +0530

    productTab

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['toprisebucket.s3.eu-north-1.amazonaws.com', 'firebasestorage.googleapis.com'],
+  },
   /* config options here */
 };
 
