0016afe3d96996f84a3916838897357e57b1f4d5
Flutterneu <rsmx141@gmail.com>
2025-08-05 12:58:03 +0530

    feat: refactor authentication types and update loginUser function to use new User interface

--- src/service/auth-service.ts
+++ src/service/auth-service.ts
@@ -1,8 +1,8 @@
 import { ApiResponse } from "@/types/apiReponses-Types";
 import apiClient from "@/apiClient";
-import { LoginRequest, LoginResponse } from "@/types/auth-types";
+import { User, LoginResponse } from "@/types/authentication-Types";
 
-export async function loginUser(data: LoginRequest): Promise<LoginResponse> {
+export async function loginUser(data: User): Promise<LoginResponse> {
   const response = await apiClient.post("/users/api/users/loginWeb", data, {
     withCredentials: true,
   });
