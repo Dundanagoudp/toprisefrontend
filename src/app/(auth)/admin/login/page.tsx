5bb367cc04a97574694546a9759c8e5201ca5349
Uttam Shetty <uttamshetty126@gmail.com>
2025-08-25 12:48:49 +0530

    feat: Implement user login and signup forms with validation and routing

--- src/app/(auth)/admin/login/page.tsx
+++ src/app/(auth)/admin/login/page.tsx
@@ -1,10 +1,10 @@
-import {  UserSignUpForm } from "@/components/login-flow/login-create"
+import { LoginForm } from "@/components/login-flow/login-form";
 
-export default function Signup() {
+export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <UserSignUpForm/>
+        <LoginForm />
       </div>
     </div>
   )
